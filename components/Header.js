5af45cb095d059eb353bfe4faa127ef74223367b
Aoying Zhang <107318892+AoyingZhang@users.noreply.github.com>
2023-06-01 00:04:24 -0400

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <header className="header">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_P93tv8yKjD7V5vqLpiPIZYnM35mt9xKIqg&usqp=CAU"/>
+            <h2>Meme Generator</h2>
+        </header>
+    )
+}
